ad81c381cd68d03874b6baf21a2633806e1b1564
Weber Dima <weber.da@mail.ru>
2023-06-03 01:03:37 +0300

    1 ревью
    Проект для отправки на 1 ревью

---
+++ index.js
@@ -0,0 +1,28 @@
+const popup = document.querySelector('.popup');
+const profileName = document.querySelector('.profile__name');
+const profileProfession = document.querySelector('.profile__profession');
+const close = document.querySelector('.popup__container_close-icon');
+const buttclick = document.querySelector('.profile__edit-button');
+const saveButton = document.querySelector('.popup__container_submit-button');
+let formElement = document.querySelector('.popup__container');
+let nameInput = document.querySelector('.popup__container_field-name');
+let jobInput = document.querySelector('.popup__container_field-profession');
+
+buttclick.addEventListener('click', function () {
+  popup.classList.add('popup_opened');
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileProfession.textContent;
+});
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileProfession.textContent = jobInput.value;
+  popup.classList.remove('popup_opened');
+};
+
+close.addEventListener('click', function () {
+  popup.classList.remove('popup_opened');
+});
+
+formElement.addEventListener('submit', handleFormSubmit);
