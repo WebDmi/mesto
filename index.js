9328a791068be1457f54630841b5dce49a2328a9
Weber Dima <weber.da@mail.ru>
2023-06-03 13:50:25 +0300

    ! ревью
    Исправлены наименования по БЭМ

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const popup = document.querySelector('.popup');
 const profileName = document.querySelector('.profile__name');
 const profileProfession = document.querySelector('.profile__profession');
-const close = document.querySelector('.popup__container_close-icon');
+const close = document.querySelector('.popup__close-icon');
 const buttclick = document.querySelector('.profile__edit-button');
-const saveButton = document.querySelector('.popup__container_submit-button');
+const saveButton = document.querySelector('.popup__submit-button');
 let formElement = document.querySelector('.popup__container');
-let nameInput = document.querySelector('.popup__container_field-name');
-let jobInput = document.querySelector('.popup__container_field-profession');
+let nameInput = document.querySelector('.popup__field-name');
+let jobInput = document.querySelector('.popup__field-profession');
 
 buttclick.addEventListener('click', function () {
   popup.classList.add('popup_opened');
